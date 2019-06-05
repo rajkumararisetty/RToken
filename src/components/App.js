20e95a1c5b79b5dd23205c88d78e6a8ffac93611
rajkumararisetty <rajkumararisetty1993@gmail.com>
2019-06-05 23:25:14 +0530

    basic web3 setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,115 +1,114 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Web3 from 'web3'
 import Token from '../abis/Token.json'
 
-class App extends Component {
-    componentWillMount() {
-        this.loadBlockchainData()
-    }
+const App = () => {
+    // Effect for loading the blockchain data
+    useEffect(() => {
+        const loadBlockchainData = async () => {
+            const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
+            const network = await web3.eth.net.getNetworkType()
+            const networkId = await web3.eth.net.getId()
+            const accounts = await web3.eth.getAccounts()
+            const abi = Token.abi;
+            const address = Token.networks[networkId]['address'];
+            const networks = Token.networks;
+            const token = web3.eth.Contract(abi, address);
+            const totalSupply = await token.methods.totalSupply().call();
+            console.log(accounts);
+        };
 
-    async loadBlockchainData() {
-        const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
-        const network = await web3.eth.net.getNetworkType()
-        const networkId = await web3.eth.net.getId()
-        const accounts = await web3.eth.getAccounts()
-        const abi = Token.abi;
-        const address = Token.networks[networkId]['address'];
-        const networks = Token.networks;
-        const token = web3.eth.Contract(abi, address);
-        const totalSupply = await token.methods.totalSupply().call();
-        console.log(accounts);
-    }
+        loadBlockchainData();
+    }, []);
 
-    render() {
-        return (
-            <div>
+    return (
+        <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                  <a className="navbar-brand" href="/#">Navbar</a>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                      <li className="nav-item">
-                        <a className="nav-link" href="/#">Link 1</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="/#">Link 2</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="/#">Link 3</a>
-                      </li>
-                    </ul>
-                  </div>
+                    <a className="navbar-brand" href="/#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/#">Link 1</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/#">Link 2</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/#">Link 3</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
                 <div className="content">
-                  <div className="vertical-split">
-                    <div className="card bg-dark text-white">
-                      <div className="card-header">
-                        Card Title
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/#" className="card-link">Card link</a>
-                      </div>
+                    <div className="vertical-split">
+                        <div className="card bg-dark text-white">
+                            <div className="card-header">
+                                Card Title
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="/#" className="card-link">Card link</a>
+                            </div>
+                        </div>
+                        <div className="card bg-dark text-white">
+                            <div className="card-header">
+                                Card Title
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="/#" className="card-link">Card link</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card bg-dark text-white">
-                      <div className="card-header">
-                        Card Title
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/#" className="card-link">Card link</a>
-                      </div>
+                    <div className="vertical">
+                        <div className="card bg-dark text-white">
+                            <div className="card-header">
+                                Card Title
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="/#" className="card-link">Card link</a>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div className="vertical">
-                    <div className="card bg-dark text-white">
-                      <div className="card-header">
-                        Card Title
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/#" className="card-link">Card link</a>
-                      </div>
+                    <div className="vertical-split">
+                        <div className="card bg-dark text-white">
+                            <div className="card-header">
+                                Card Title
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="/#" className="card-link">Card link</a>
+                            </div>
+                        </div>
+                        <div className="card bg-dark text-white">
+                            <div className="card-header">
+                                Card Title
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="/#" className="card-link">Card link</a>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div className="vertical-split">
-                    <div className="card bg-dark text-white">
-                      <div className="card-header">
-                        Card Title
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/#" className="card-link">Card link</a>
-                      </div>
+                    <div className="vertical">
+                        <div className="card bg-dark text-white">
+                            <div className="card-header">
+                                Card Title
+                            </div>
+                            <div className="card-body">
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="/#" className="card-link">Card link</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card bg-dark text-white">
-                      <div className="card-header">
-                        Card Title
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/#" className="card-link">Card link</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="vertical">
-                    <div className="card bg-dark text-white">
-                      <div className="card-header">
-                        Card Title
-                      </div>
-                      <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="/#" className="card-link">Card link</a>
-                      </div>
-                    </div>
-                  </div>
                 </div>
             </div>
         );
-    }
 }
 
 export default App;
