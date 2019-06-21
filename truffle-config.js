16802a8c785fccb51b862be3b57af4442bdcd09c
rajkumararisetty <rajkumararisetty1993@gmail.com>
2019-06-21 11:54:18 +0530

    Configured truffle for kovan test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,25 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || "";
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` // URL to an etherum node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     }
   },
   contracts_directory: './src/contracts/',
